76b1531f8c8f3cb39924df6c8e7c259e04cd937c
Amy Kim <yk154@duke.edu>
2019-11-03 12:35:48 -0500

    connection of pq, and WIP on queries

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors'
+import bodyParser from 'body-parser';
 
 const app = express();
+const port = 3000;
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
 
 app.use(cors());
 
