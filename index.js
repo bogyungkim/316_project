5ecd4e43d6eeae940462fe61360352dafcf230a5
John Kang <jhk9503@gmail.com>
2019-11-03 12:30:36 -0500

    add cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors'
 
 const app = express();
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   return res.send('Received a GET HTTP method');
