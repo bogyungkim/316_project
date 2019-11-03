d43fc65b8cdfcdcd3a800f240eb24bfda35a1887
bk141 <bk141@duke.edu>
2019-11-03 15:55:16 -0500

    get ok but create no

--- models/queries.js
+++ models/queries.js
@@ -19,16 +19,27 @@ const getUsers = (request, response) => {
 };
 
 const createUser = (request, response) => {
-  const { uid, phoneNumber, password, level, netid, deletedAt } = request.body;
+  //const { uid, phoneNumber, password, level, netid, deletedAt } = request.body;
 
-  pool.query('', (error, result) => {
-    'INSERT INTO users (uid, phoneNumber, password, level, netid, deletedAt) VALUES' +
-        '(2, \$\'9195648686\', \$\'123123\', \$1, \$\'yk154\', \$NULL), [uid, phoneNumber, password, level, netid, deletedAt]'
-    if (error) {
-      response.status(400).send(`Error detected ${error}`);
-    }
-    response.status(201).send(`User added ${result}`);
-  });
+  // pool.query('', (error, result) => {
+  //   // 'INSERT INTO users (uid, phoneNumber, password, level, netid, deletedAt) VALUES' +
+  //   //     '(2, \$\'9195648686\', \$\'123123\', \$1, \$\'yk154\', \$NULL), [uid, phoneNumber, password, level, netid, deletedAt]'
+  //   if (error) {
+  //     response.status(400).send(`Error detected ${error}`);
+  //   }
+  //   response.status(201).send(`User added ${result}`);
+  // });
+  
+  
+  //pool.query("INSERT INTO users (uid, phoneNumber, password, level, netid, deletedAt) values ($1, $2, $3, $4, $5, $6) ", [2, '9195648686', '123123', 1, 'yk154', NULL], callBack)
+
+  var format = require('pg-format');
+
+  var values = [
+    [2, '9195648686', '123123', 1, 'yk154', NULL]
+  ];
+  console.log(format('INSERT INTO users (uid, phoneNumber, password, level, netid, deletedAt) VALUES %L', values));
+
 };
 
 export default {
