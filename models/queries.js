b1532e6640d47f1fcf7bb4b22594c65c06671117
Amy Kim <yk154@duke.edu>
2019-11-03 16:45:25 -0500

    WIP

--- models/queries.js
+++ models/queries.js
@@ -9,7 +9,6 @@ const pool = new Pool({
 });
 
 const getUsers = (request, response) => {
-  // response.send("hello")
   pool.query('SELECT * FROM users', (error, results) => {
     if (error) {
       response.status(400).json(error);
@@ -19,30 +18,36 @@ const getUsers = (request, response) => {
 };
 
 const createUser = (request, response) => {
-  //const { uid, phoneNumber, password, level, netid, deletedAt } = request.body;
+  const { uid, phoneNumber, password, level, netid, deletedAt } = request.body
 
-  // pool.query('', (error, result) => {
-  //   // 'INSERT INTO users (uid, phoneNumber, password, level, netid, deletedAt) VALUES' +
-  //   //     '(2, \$\'9195648686\', \$\'123123\', \$1, \$\'yk154\', \$NULL), [uid, phoneNumber, password, level, netid, deletedAt]'
-  //   if (error) {
-  //     response.status(400).send(`Error detected ${error}`);
-  //   }
-  //   response.status(201).send(`User added ${result}`);
-  // });
-  
-  
-  //pool.query("INSERT INTO users (uid, phoneNumber, password, level, netid, deletedAt) values ($1, $2, $3, $4, $5, $6) ", [2, '9195648686', '123123', 1, 'yk154', NULL], callBack)
+  pool.query('INSERT INTO users (uid, phoneNumber, password, level, netid, deletedAt) VALUES ($1, $2, $3, $4, $5, $6)', [uid, phoneNumber, password, level, netid, deletedAt], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).send(`User added with ID: ${results}`);
+    response.status(200).json(results);
+  })
+}
 
-  var format = require('pg-format');
+const updateUser = (request, response) => {
+  const id = parseInt(request.params.id)
+  const { phoneNumber, password, level, netid, deletedAt } = request.body
 
-  var values = [
-    [2, '9195648686', '123123', 1, 'yk154', NULL]
-  ];
-  console.log(format('INSERT INTO users (uid, phoneNumber, password, level, netid, deletedAt) VALUES %L', values));
-
-};
+  pool.query(
+    'UPDATE users SET phoneNumber = $2, password = $3, level = $4, netid = $5, deletedAt = $6 WHERE uid = ${id}',
+    [phoneNumber, password, level, netid, deletedAt],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`User modified with ID: ${id}`);
+      response.status(200).json(results);
+    }
+  )
+}
 
 export default {
   getUsers,
   createUser,
+  updateUser,
 };
